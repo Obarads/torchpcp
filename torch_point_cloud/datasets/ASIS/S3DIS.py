21404080dd5949afd9ced045c73a91700e492b67
obarads <id115561@gmail.com>
2020-12-10 01:32:32 +0000

    update dataset

--- torch_point_cloud/datasets/ASIS/S3DIS.py
+++ torch_point_cloud/datasets/ASIS/S3DIS.py
@@ -17,6 +17,10 @@ from torch_point_cloud.datasets.ASIS.utils.indoor3d_util import (
 from torch_point_cloud.utils.converter import (
     batch_sparseLabel_to_denseLabel, sparseLabel_to_denseLabel)
 
+# for Preprocessing
+from .utils.data_prep_util import save_h5ins
+from .utils.indoor3d_util import collect_point_label
+
 def create_batch_instance_information(batch_ins_labels):
     """
     create new instance labels, masks, number of instances list and maximum number of instance. 
@@ -268,3 +272,73 @@ def get_block_paths(root, areas, extension=".h5"):
                 h5_file_path_list.append(h5_file_path)
 
     return h5_file_path_list
+
+class Preprocessing:
+    def __init__(self):
+        print("This class is for staticmethod.")
+
+    @staticmethod
+    def create_scene_dataset(dataset_root, output_root):
+        anno_relative_paths = [line.rstrip() for line in open(os.path.join(BASE_DIR, 'utils/meta/S3DIS/anno_paths.txt'))]
+        anno_absolute_paths = [os.path.join(dataset_root, p) for p in anno_relative_paths]
+
+        g_classes = [x.rstrip() for x in open(os.path.join(BASE_DIR, 'utils/meta/S3DIS/class_names.txt'))]
+        g_class2label = {cls: i for i,cls in enumerate(g_classes)}
+
+        if not os.path.exists(output_root):
+            os.mkdir(output_root)
+
+        anno_absolute_paths = tqdm(anno_absolute_paths, ncols=65)
+        # Note: there is an extra character in the v1.2 data in Area_5/hallway_6. It's fixed manually.
+        for anno_path in anno_absolute_paths:
+            try:
+                elements = anno_path.split('/')
+                out_filename = elements[-3]+'_'+elements[-2]+'.npy' # npy only
+                output_path = os.path.join(output_root, out_filename)
+                if os.path.exists(output_path) == False:
+                    collect_point_label(anno_path, output_path, file_format='numpy')
+            except:
+                print(anno_path, 'ERROR!!')
+
+    @staticmethod
+    def create_block_dataset(dataset_root, output_root, num_points, block_size, 
+                            stride):
+        # Constants
+        indoor3d_data_dir = dataset_root
+        NUM_POINT = num_points
+        data_dtype = 'float32'
+        label_dtype = 'int32'
+
+        # Set paths
+        filelist = os.path.join(BASE_DIR, 'utils/meta/S3DIS/all_data_label.txt')
+        data_label_files = [os.path.join(indoor3d_data_dir, line.rstrip()) for 
+                            line in open(filelist)]
+        output_dir = output_root
+        if not os.path.exists(output_dir):
+            os.mkdir(output_dir)
+        output_room_filelist = os.path.join(output_dir, 'room_filelist.txt')
+        fout_room = open(output_room_filelist, 'w')
+
+        sample_cnt = 0
+        for i in range(0, len(data_label_files)):
+            data_label_filename = data_label_files[i]
+            fname = os.path.basename(data_label_filename).strip('.npy')
+            if not os.path.exists(data_label_filename):
+                continue
+            data, label, inslabel = room2blocks_wrapper_normalized(
+                data_label_filename, NUM_POINT, block_size=block_size, 
+                stride=stride, random_sample=False, sample_num=None)
+            for _ in range(data.shape[0]):
+                fout_room.write(os.path.basename(data_label_filename)[0:-4]+'\n')
+
+            sample_cnt += data.shape[0]
+            h5_filename = os.path.join(output_dir, '%s.h5' % fname)
+            print('{0}: {1}, {2}, {3}'.format(h5_filename, data.shape,  
+                                            label.shape, inslabel.shape))
+            save_h5ins(h5_filename, data, label, inslabel, data_dtype, label_dtype)
+
+        fout_room.close()
+        print("Total samples: {0}".format(sample_cnt))
+
+
+
